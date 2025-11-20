8087f01c40f174ca5cfa12e807c437c227b3f8c1
SzXPeter <szabo.peter@students.jedlik.eu>
2025-11-20 11:54:00 +0100

    Added Class class

---
+++ Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kreta_WPF
+{
+    internal class Class
+    {
+        public Class(Dictionary<Subject, Teacher> SubjectsAndTeachers, List<Student> Students)
+        {
+            this.SubjectsAndTeachers = SubjectsAndTeachers;
+
+            foreach (var item in SubjectsAndTeachers)
+            {
+                Subjects.Add(item.Key);
+                if (!Teachers.Contains(item.Value))
+                    Teachers.Add(item.Value);
+            }
+
+            this.Students = Students;
+        }
+        public Dictionary<Subject, Teacher> SubjectsAndTeachers { get; set; }
+        public List<Subject> Subjects { get; set; } = [];
+        public List<Teacher> Teachers { get; set; } = [];
+        public List<Student> Students { get; set; }
+    }
+}
