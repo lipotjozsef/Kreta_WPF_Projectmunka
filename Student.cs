9a6387834a687a70f40310fd2a6d4ddefb798752
SzXPeter <szabo.peter@students.jedlik.eu>
2025-11-20 11:54:16 +0100

    Added Student class

---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kreta_WPF
+{
+    internal class Student(int ID, string Name, string Password, List<Subject> Subjects) : User(ID, Name, Password)
+    {
+        public List<Subject> Subjects { get; set; } = Subjects;
+
+        public double AverageMark()
+        {
+            return Subjects.Average(x => x.AverageMark());
+        }
+    }
+}
