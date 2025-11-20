89843c2fcc949fefe10e0453c0d2ca114d7fdd1f
lipotjozsef <lipot.jozsef.zoltan@students.jedlik.eu>
2025-11-20 12:26:04 +0100

    Window Change code started

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,27 @@ namespace Kreta_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public IEnumerable<FrameworkElement>? Frames = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            Frames = getElementsByTag(this, "contentFrame");
+            foreach (var f in Frames) Debug.WriteLine(f.Name);
+        }
+
+        public IEnumerable<FrameworkElement> getElementsByTag(DependencyObject parent, object tag)
+        {
+            IEnumerable<FrameworkElement> elements = new List<FrameworkElement>();
+
+            foreach(var chil in LogicalTreeHelper.GetChildren(parent))
+            {
+                if (chil is FrameworkElement fe)
+                {
+                    if (Equals(fe.Tag, tag)) elements.Append(chil);
+                }
+            }
+            return elements;
         }
     }
 }
