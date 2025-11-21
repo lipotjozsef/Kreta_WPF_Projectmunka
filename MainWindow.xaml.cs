94abf54874f4e0bc996df2eb820e7c0a2735cb18
lipotjozsef <lipot.jozsef.zoltan@students.jedlik.eu>
2025-11-21 20:15:10 +0100

    Added frame change functionality: - getElementsByTag() - menuButtonClick() - changeActiveFrameTo()

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,21 +23,40 @@ namespace Kreta_WPF
         {
             InitializeComponent();
             Frames = getElementsByTag(this, "contentFrame");
-            foreach (var f in Frames) Debug.WriteLine(f.Name);
+            Debug.WriteLine("\tPrinting Frames");
+            foreach (var f in Frames) Debug.WriteLine($"\t\t{f.Name}");
         }
 
         public IEnumerable<FrameworkElement> getElementsByTag(DependencyObject parent, object tag)
         {
-            IEnumerable<FrameworkElement> elements = new List<FrameworkElement>();
+            var elements = new List<FrameworkElement>();
 
-            foreach(var chil in LogicalTreeHelper.GetChildren(parent))
+            foreach (var child in LogicalTreeHelper.GetChildren(parent))
             {
-                if (chil is FrameworkElement fe)
+                if (child is FrameworkElement fe)
                 {
-                    if (Equals(fe.Tag, tag)) elements.Append(chil);
+                    if (Equals(fe.Tag, tag)) elements.Add(fe);
+
+                    elements.AddRange(getElementsByTag(fe, tag));
                 }
             }
             return elements;
+        }
+
+        private void menuButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is null) return;
+            Button? senderBtn = sender as Button;
+            changeActiveFrameTo(senderBtn?.Tag);
+        }
+
+        private void changeActiveFrameTo(object? activeName)
+        {
+            if (Frames is null || activeName is null) return;
+            foreach(FrameworkElement f in Frames)
+            {
+                f.Visibility = Equals(f.Name, activeName) ? Visibility.Visible : Visibility.Hidden;
+            }
         }
     }
 }
