361fd77a4279929c1d584bac88531b9dab2d1ccd
lipotjozsef <lipot.jozsef.zoltan@students.jedlik.eu>
2025-11-22 18:39:36 +0100

    Added time table code: - loadTimeTable() - generateRowsColumnsByTag()

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
 using System.Security.AccessControl;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -26,8 +28,8 @@ namespace Kreta_WPF
         {
             InitializeComponent();
             Frames = getElementsByTag(this, "contentFrame");
-            Debug.WriteLine("\tPrinting Frames");
-            foreach (var f in Frames) Debug.WriteLine($"\t\t{f.Name}");
+            /*Debug.WriteLine("\tPrinting Frames");
+            foreach (var f in Frames) Debug.WriteLine($"\t\t{f.Name}");*/
         }
 
         public IEnumerable<FrameworkElement> getElementsByTag(DependencyObject parent, object tag)
@@ -109,6 +111,49 @@ namespace Kreta_WPF
         {
             TextBlock? myObject = sender as TextBlock;
             if(myObject is TextBlock) myObject.Text = "";
+        }
+
+        private void loadTimeTable(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Temp Only!
+            string[] possibleClasses = ["Matematika", "Történelem", "Adatbázis", "Asztali Alkalmazások Fejlesztése", "Backend", "Nyelvtan", "Irodalom", "Testnevelés", "Angol", "Web fejlesztés"];
+            Random myRandom = new Random();
+            UniformGrid? timeTable = sender as UniformGrid;
+            if (timeTable is null) return;
+
+            if (Equals(e.NewValue, true))
+            {
+                bool isStriped = false;
+                for (int i = 0; i != 9; i++)
+                {
+                    for (int j = 0; j != 5; j++)
+                    {
+                        Label newClassLabel = new Label
+                        {
+                            HorizontalContentAlignment = HorizontalAlignment.Center,
+                            VerticalContentAlignment = VerticalAlignment.Center,
+                            Style = (Style)ttTable.Resources["baseLabel"],
+                            Content = possibleClasses[myRandom.Next(possibleClasses.Length)]
+                        };
+                        timeTable.Children.Add(newClassLabel);
+                        if (isStriped) newClassLabel.Background = Brushes.WhiteSmoke;
+                    }
+                    isStriped = !isStriped;
+                }
+            }
+            else timeTable.Children.Clear();
+        }
+
+        private void generateRowsColumnsByTag(object sender, EventArgs e)
+        {
+            Grid? generateGrid = sender as Grid;
+            if (generateGrid is null && generateGrid?.Tag is null) return;
+            string[] parts = generateGrid.Tag.ToString().Split('_');
+            (int rowNum, int colNum) = (int.Parse(parts[0]), int.Parse(parts[1]));
+            for (int i = 0; i != rowNum; i++)
+                generateGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+            for (int j = 0; j != colNum; j++)
+                generateGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
         }
     }
 }
