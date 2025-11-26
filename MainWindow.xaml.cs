e169e42545e0120bc3579a9c29cace072ef28da8
lipotjozsef <lipot.jozsef.zoltan@students.jedlik.eu>
2025-11-26 14:10:05 +0100

    UI elements functionality added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,8 @@
-﻿using System.Diagnostics;
-using System.Diagnostics.Eventing.Reader;
-using System.Security.AccessControl;
-using System.Text;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Kreta_WPF
 {
@@ -23,6 +12,9 @@ namespace Kreta_WPF
     public partial class MainWindow : Window
     {
         public IEnumerable<FrameworkElement>? Frames = null;
+        // Temp Only!
+        string[] possibleClasses = ["Matematika", "Történelem", "Adatbázis", "Asztali Alkalmazások Fejlesztése", "Backend", "Nyelvtan", "Irodalom", "Testnevelés", "Angol", "Web fejlesztés"];
+        Random myRandom = new Random();
 
         public MainWindow()
         {
@@ -115,15 +107,11 @@ namespace Kreta_WPF
 
         private void loadTimeTable(object sender, DependencyPropertyChangedEventArgs e)
         {
-            // Temp Only!
-            string[] possibleClasses = ["Matematika", "Történelem", "Adatbázis", "Asztali Alkalmazások Fejlesztése", "Backend", "Nyelvtan", "Irodalom", "Testnevelés", "Angol", "Web fejlesztés"];
-            Random myRandom = new Random();
             UniformGrid? timeTable = sender as UniformGrid;
             if (timeTable is null) return;
 
             if (Equals(e.NewValue, true))
             {
-                bool isStriped = false;
                 for (int i = 0; i != 9; i++)
                 {
                     for (int j = 0; j != 5; j++)
@@ -133,15 +121,138 @@ namespace Kreta_WPF
                             HorizontalContentAlignment = HorizontalAlignment.Center,
                             VerticalContentAlignment = VerticalAlignment.Center,
                             Style = (Style)ttTable.Resources["baseLabel"],
+                            Background = i % 2 == 0 ? Brushes.WhiteSmoke : Brushes.White,
                             Content = possibleClasses[myRandom.Next(possibleClasses.Length)]
                         };
                         timeTable.Children.Add(newClassLabel);
-                        if (isStriped) newClassLabel.Background = Brushes.WhiteSmoke;
                     }
-                    isStriped = !isStriped;
                 }
             }
             else timeTable.Children.Clear();
+        }
+
+        private void loadGrades(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            StackPanel? gradePanel = sender as StackPanel;
+            if (gradePanel is null) return;
+
+            if (Equals(e.NewValue, true))
+            {
+                for(int i = 0; i != possibleClasses.Length; i++)
+                {
+                    UniformGrid generatedGradeCard = generateGradeCard(i, possibleClasses[i]);
+                    gradePanel.Children.Add(generatedGradeCard);
+                }
+            }
+            else gradePanel.Children.Clear();
+        }
+
+        private void loadAbsences(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            StackPanel? absencePanel = sender as StackPanel;
+            if(absencePanel is null) return;
+
+            if (Equals(e.NewValue, true))
+            {
+                Border generatedBorder = generateNewAbsenceCard();
+                absencePanel.Children.Add(generatedBorder);
+            }
+            else absencePanel.Children.Clear();
+        }
+
+        private void loadHomework(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            StackPanel? homeworkPanel = sender as StackPanel;
+            if (homeworkPanel is null) return;
+
+            if(Equals(e.NewValue, true))
+            {
+                Border generatedBorder = generateNewHomeworkCard();
+                homeworkPanel.Children.Add(generatedBorder);
+            }
+            else homeworkPanel.Children.Clear();
+        }
+
+        private UniformGrid generateGradeCard(int index = 0, string content = "", bool flipColors = false)
+        {
+            UniformGrid gridParent = new UniformGrid
+            {
+                Columns = 16,
+                Rows = 1,
+                Background = index % 2 == 0 ? Brushes.WhiteSmoke : Brushes.White,
+            };
+            Label indexLabel = new Label
+            {
+                Content = (index + 1).ToString()
+            };
+            Label nameLabel = new Label
+            {
+                Content = content
+            };
+
+            gridParent.Children.Add(indexLabel);
+            gridParent.Children.Add(nameLabel);
+
+            for (int j = 0; j != 14; j++)
+            {
+                Label gradeLabel = new Label
+                {
+                    Content = "-"
+                };
+                gridParent.Children.Add(gradeLabel);
+            }
+            return gridParent;
+        }
+
+        private Border generateNewAbsenceCard()
+        {
+            Border parentBorder = generateNewCard(["November 6", "Csütörtök", "-"]);
+            return parentBorder;
+        }
+
+        private Border generateNewHomeworkCard()
+        {
+            Border parentBorder = generateNewCard(["2025. 11. 26", "Fizika", "TK 89/ 1. 2. 3."]);
+            return parentBorder;
+        }
+
+        private Border generateNewCard(string[] contents)
+        {
+            Border parentBorder = new Border();
+            UniformGrid parentGrid = new UniformGrid()
+            {
+                Columns = 2,
+                Rows = 1
+            };
+            parentBorder.Child = parentGrid;
+            StackPanel stackParent = new StackPanel
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            parentGrid.Children.Add(stackParent);
+            Label title = new Label()
+            {
+                Content = contents[0]
+            };
+            Label title2 = new Label()
+            {
+                Content = contents[1]
+            };
+            stackParent.Children.Add(title);
+            stackParent.Children.Add(title2);
+
+            Label desc = new Label()
+            {
+                Content = contents[2],
+                HorizontalContentAlignment = HorizontalAlignment.Right,
+                VerticalContentAlignment = VerticalAlignment.Center,
+                Background = Brushes.Gray,
+                Foreground = Brushes.WhiteSmoke
+
+            };
+            parentGrid.Children.Add(desc);
+            return parentBorder;
         }
 
         private void generateRowsColumnsByTag(object sender, EventArgs e)
