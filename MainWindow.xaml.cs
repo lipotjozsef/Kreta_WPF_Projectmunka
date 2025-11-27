9c2aeb194994bd987d53a9bd163e633b5e6131d4
lipotjozsef <lipot.jozsef.zoltan@students.jedlik.eu>
2025-11-27 12:27:26 +0100

    Actual login functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kreta_WPF.Pages;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -11,20 +12,26 @@ namespace Kreta_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        public IEnumerable<FrameworkElement>? Frames = null;
-        // Temp Only!
-        string[] possibleClasses = ["Matematika", "Történelem", "Adatbázis", "Asztali Alkalmazások Fejlesztése", "Backend", "Nyelvtan", "Irodalom", "Testnevelés", "Angol", "Web fejlesztés"];
-        Random myRandom = new Random();
+        static string studentPath = "./JSONData/Students.json";
+        static string teacherPath = "./JSONData/Teachers.json";
+        static string classesPath = "./JSONData/Classes.json";
+        static List<Student> students = User.ReadUsers<Student>(studentPath);
+        static List<Teacher> teachers = User.ReadUsers<Teacher>(teacherPath);
+        static List<Class> classes = Class.ReadClasses(classesPath);
 
+        Student? loggedinUser = null;
         public MainWindow()
         {
             InitializeComponent();
-            Frames = getElementsByTag(this, "contentFrame");
+            foreach(Class myClass in classes)
+            {
+                myClass.LoadSubjects(students);
+            }
             /*Debug.WriteLine("\tPrinting Frames");
             foreach (var f in Frames) Debug.WriteLine($"\t\t{f.Name}");*/
         }
 
-        public IEnumerable<FrameworkElement> getElementsByTag(DependencyObject parent, object tag)
+        static public IEnumerable<FrameworkElement> getElementsByTag(DependencyObject parent, object tag)
         {
             var elements = new List<FrameworkElement>();
 
@@ -57,214 +64,56 @@ namespace Kreta_WPF
 
             try
             {
-                string username = loginUsername.Text;
+                int userid = -1;
+                int.TryParse(loginUserID.Text, out userid);
                 string password = loginPassword.Password;
 
-                if (string.IsNullOrEmpty(username)) writeError("A felhasználó név megadása kötelező a bejelentkezéshez!", loginUsername);
+                if (userid == -1) writeError("A felhasználó név megadása kötelező a bejelentkezéshez!", loginUserID);
                 if (string.IsNullOrEmpty(password)) writeError("A jelszó megadása kötelező a bejelentkezéshez!", loginPassword);
 
-                bool loggedIn = tryLogin(username, password);
+                bool loggedIn = tryLogin(userid, password);
                 if (loggedIn)
                 {
                     loginPage.Visibility = Visibility.Collapsed;
                     activePage.Visibility = Visibility.Visible;
+                    Page? newMainPage = null;
+                    /*if (loggedinUser is Teacher) newMainPage = new TeacherPage(loggedinUser as Teacher);
+                    else if(loggedinUser is Student) newMainPage = new StudentPage(loggedinUser as Student);
+
+                    MainFrame.Navigate(newMainPage);*/
+                    MainFrame.Navigate(new StudentPage(loggedinUser));
                 }
                 else writeError("A megadott felhasználónév/jelszó párral nem található felhasználó a rendszerben.", null);
             }
             catch (ArgumentNullException) { }
         }
 
-        private bool secondOnlyLogin = false;
-        private bool tryLogin(string us, string ps)
+        private bool tryLogin(int id, string ps)
         {
-            // TODO ADD LOGIN FUNCTIONALITY
-            bool returnVal = secondOnlyLogin;
-            secondOnlyLogin = !secondOnlyLogin;
-            return returnVal;
-        }
-
-        private void menuButtonClick(object sender, RoutedEventArgs e)
-        {
-            if (sender is null) return;
-            Button? senderBtn = sender as Button;
-            changeActiveFrameTo(senderBtn?.Tag);
-        }
-
-        private void changeActiveFrameTo(object? activeName)
-        {
-            if (Frames is null || activeName is null) return;
-            foreach(FrameworkElement f in Frames)
-            {
-                f.Visibility = Equals(f.Name, activeName) ? Visibility.Visible : Visibility.Collapsed;
+            foreach (Student user in students) {
+                if (user.Login(id, ps))
+                {
+                    loggedinUser = user;
+                    return true;
+                }
             }
+
+            /*List<Teacher> teachers = User.ReadUsers<Teacher>(teacherPath);
+
+            foreach (User user in teachers) {
+                if (user.Login(id, ps))
+                {
+                    loggedinUser = user;
+                    return true;
+                }
+            }*/
+            return false;
         }
 
         private void emptyLabel(object sender, RoutedEventArgs e)
         {
             TextBlock? myObject = sender as TextBlock;
             if(myObject is TextBlock) myObject.Text = "";
-        }
-
-        private void loadTimeTable(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            UniformGrid? timeTable = sender as UniformGrid;
-            if (timeTable is null) return;
-
-            if (Equals(e.NewValue, true))
-            {
-                for (int i = 0; i != 9; i++)
-                {
-                    for (int j = 0; j != 5; j++)
-                    {
-                        Label newClassLabel = new Label
-                        {
-                            HorizontalContentAlignment = HorizontalAlignment.Center,
-                            VerticalContentAlignment = VerticalAlignment.Center,
-                            Style = (Style)ttTable.Resources["baseLabel"],
-                            Background = i % 2 == 0 ? Brushes.WhiteSmoke : Brushes.White,
-                            Content = possibleClasses[myRandom.Next(possibleClasses.Length)]
-                        };
-                        timeTable.Children.Add(newClassLabel);
-                    }
-                }
-            }
-            else timeTable.Children.Clear();
-        }
-
-        private void loadGrades(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            StackPanel? gradePanel = sender as StackPanel;
-            if (gradePanel is null) return;
-
-            if (Equals(e.NewValue, true))
-            {
-                for(int i = 0; i != possibleClasses.Length; i++)
-                {
-                    UniformGrid generatedGradeCard = generateGradeCard(i, possibleClasses[i]);
-                    gradePanel.Children.Add(generatedGradeCard);
-                }
-            }
-            else gradePanel.Children.Clear();
-        }
-
-        private void loadAbsences(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            StackPanel? absencePanel = sender as StackPanel;
-            if(absencePanel is null) return;
-
-            if (Equals(e.NewValue, true))
-            {
-                Border generatedBorder = generateNewAbsenceCard();
-                absencePanel.Children.Add(generatedBorder);
-            }
-            else absencePanel.Children.Clear();
-        }
-
-        private void loadHomework(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            StackPanel? homeworkPanel = sender as StackPanel;
-            if (homeworkPanel is null) return;
-
-            if(Equals(e.NewValue, true))
-            {
-                Border generatedBorder = generateNewHomeworkCard();
-                homeworkPanel.Children.Add(generatedBorder);
-            }
-            else homeworkPanel.Children.Clear();
-        }
-
-        private UniformGrid generateGradeCard(int index = 0, string content = "", bool flipColors = false)
-        {
-            UniformGrid gridParent = new UniformGrid
-            {
-                Columns = 16,
-                Rows = 1,
-                Background = index % 2 == 0 ? Brushes.WhiteSmoke : Brushes.White,
-            };
-            Label indexLabel = new Label
-            {
-                Content = (index + 1).ToString()
-            };
-            Label nameLabel = new Label
-            {
-                Content = content
-            };
-
-            gridParent.Children.Add(indexLabel);
-            gridParent.Children.Add(nameLabel);
-
-            for (int j = 0; j != 14; j++)
-            {
-                Label gradeLabel = new Label
-                {
-                    Content = "-"
-                };
-                gridParent.Children.Add(gradeLabel);
-            }
-            return gridParent;
-        }
-
-        private Border generateNewAbsenceCard()
-        {
-            Border parentBorder = generateNewCard(["November 6", "Csütörtök", "-"]);
-            return parentBorder;
-        }
-
-        private Border generateNewHomeworkCard()
-        {
-            Border parentBorder = generateNewCard(["2025. 11. 26", "Fizika", "TK 89/ 1. 2. 3."]);
-            return parentBorder;
-        }
-
-        private Border generateNewCard(string[] contents)
-        {
-            Border parentBorder = new Border();
-            UniformGrid parentGrid = new UniformGrid()
-            {
-                Columns = 2,
-                Rows = 1
-            };
-            parentBorder.Child = parentGrid;
-            StackPanel stackParent = new StackPanel
-            {
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
-            };
-            parentGrid.Children.Add(stackParent);
-            Label title = new Label()
-            {
-                Content = contents[0]
-            };
-            Label title2 = new Label()
-            {
-                Content = contents[1]
-            };
-            stackParent.Children.Add(title);
-            stackParent.Children.Add(title2);
-
-            Label desc = new Label()
-            {
-                Content = contents[2],
-                HorizontalContentAlignment = HorizontalAlignment.Right,
-                VerticalContentAlignment = VerticalAlignment.Center,
-                Background = Brushes.Gray,
-                Foreground = Brushes.WhiteSmoke
-
-            };
-            parentGrid.Children.Add(desc);
-            return parentBorder;
-        }
-
-        private void generateRowsColumnsByTag(object sender, EventArgs e)
-        {
-            Grid? generateGrid = sender as Grid;
-            if (generateGrid is null && generateGrid?.Tag is null) return;
-            string[] parts = generateGrid.Tag.ToString().Split('_');
-            (int rowNum, int colNum) = (int.Parse(parts[0]), int.Parse(parts[1]));
-            for (int i = 0; i != rowNum; i++)
-                generateGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
-            for (int j = 0; j != colNum; j++)
-                generateGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
         }
     }
 }
