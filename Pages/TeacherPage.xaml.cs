f82a919927e7304ca65c7638b58c756970c071db
lipotjozsef <lipot.jozsef.zoltan@students.jedlik.eu>
2025-11-27 20:40:13 +0100

    Absence adder - loadStudents() - loadClasses() - tryNewAbsence() - changeMinutes() - changeMinutesLate()

--- Pages/TeacherPage.xaml.cs
+++ Pages/TeacherPage.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
+using System.Windows.Automation;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Kreta_WPF.Pages
 {
@@ -22,10 +12,11 @@ namespace Kreta_WPF.Pages
     {
         Teacher? loggedInUser;
         public IEnumerable<FrameworkElement>? Frames = null;
-
+        string[]? classDesignations = null;
         public TeacherPage(Teacher? loggedInUser)
         {
             this.loggedInUser = loggedInUser;
+            classDesignations = MainWindow.classes.Where(x => x.Teachers.Contains(loggedInUser.ID)).Select(x => x.ClassDesignation).ToArray();
             InitializeComponent();
             Frames = MainWindow.getElementsByTag(this, "contentFrame");
         }
@@ -44,6 +35,71 @@ namespace Kreta_WPF.Pages
             {
                 f.Visibility = Equals(f.Name, activeName) ? Visibility.Visible : Visibility.Collapsed;
             }
+        }
+
+        private void loadStudents(object sender, RoutedEventArgs e)
+        {
+            cbStudents.Items.Clear();
+            ComboBoxItem? typeItem = (ComboBoxItem)cbClasses.SelectedItem;
+            if (typeItem is null) return;
+            string? selectedDes = typeItem.Content.ToString();
+            if(string.IsNullOrEmpty(selectedDes)) return;
+
+            Class? selectedClass = MainWindow.classes.Where(x => x.ClassDesignation == selectedDes).ToArray()[0];
+            if (selectedClass is null) return;
+            foreach(int stuId in selectedClass.Students)
+            {
+                Student? currentStudent = MainWindow.students.Where(stu => stu.ID.Equals(stuId)).ToArray()[0];
+                if (currentStudent is null) continue;
+                ComboBoxItem newName = new ComboBoxItem
+                {
+                    Content = currentStudent.Name
+                };
+                cbStudents.Items.Add(newName);
+            }
+            cbStudents.SelectedIndex = 0;
+        }
+
+        private void loadClasses(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            ComboBox? senderBox = sender as ComboBox;
+            if (senderBox is null || classDesignations is null) return;
+            lMessage.Content = string.Empty;
+            absDp.SelectedDate = DateTime.Now;
+            if (Equals(e.NewValue, true))
+            {
+                foreach(string Desclass in classDesignations)
+                {
+                    ComboBoxItem newItem = new ComboBoxItem
+                    {
+                        Content = Desclass
+                    };
+                    senderBox.Items.Add(newItem);
+                }
+                senderBox.SelectedIndex = 0;
+            }
+            else senderBox.Items.Clear();
+        }
+
+        private void tryNewAbsence(object sender, RoutedEventArgs e)
+        {
+            lMessage.Content = "Mulasztás sikeresen rögzítve!";
+        }
+
+        private void changeMinutes(int amount)
+        {
+            int currentAmount = int.Parse(minutesLate.Text);
+            currentAmount = Math.Clamp(currentAmount + amount, 5, 45);
+            minutesLate.Text = currentAmount.ToString();
+        }
+
+        private void changeMinutesLate(object sender, RoutedEventArgs e)
+        {
+            Button? senderButton = sender as Button;
+            if (senderButton is null) return;
+            int changeAmount = 0;
+            int.TryParse(senderButton.Tag.ToString(), out changeAmount);
+            changeMinutes(changeAmount);
         }
 
         private void generateRowsColumnsByTag(object sender, EventArgs e)
