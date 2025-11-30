c388513b1d0c73faeb6d8ba1034cf5e3d5182a1b
SzXPeter <szabo.peter@students.jedlik.eu>
2025-11-30 12:30:48 +0000

    Write to file function

--- User.cs
+++ User.cs
@@ -1,5 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
 using System.IO;
+using System.Text;
 
 namespace Kreta_WPF
 {
@@ -7,7 +9,7 @@ namespace Kreta_WPF
     {
         public int ID { get; } = ID;
         public string Name { get; set; } = Name;
-        private string Password { get; set; } = Password;
+        public string Password { get; set; } = Password;
 
         public bool Login(int ID, string Password)
         {
@@ -16,9 +18,16 @@ namespace Kreta_WPF
 
         public static List<T> ReadUsers<T>(string FilePath)
         {
-            var JSONString = File.ReadAllText(FilePath);
-            var Users = JsonConvert.DeserializeObject<List<T>>(JSONString);
+            var JsonString = File.ReadAllText(FilePath);
+            var Users = JsonConvert.DeserializeObject<List<T>>(JsonString);
             return Users!;
+        }
+
+        public static void WriteUsers<T>(string FilePath,  List<T> Users)
+        {
+            var JsonString = JsonConvert.SerializeObject(Users, Formatting.Indented);
+            using var streamWriter = new StreamWriter(FilePath, false);
+            streamWriter.Write(JsonString);
         }
 
         public override bool Equals(object? obj)
